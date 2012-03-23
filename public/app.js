b5116a905a5419b7aefac08af42c40c9d6eb1179
Adriano Flach <flachadriano@gmail.com>
2012-03-23 13:21:27 -0300

    implementing viewport

--- public/app.js
+++ public/app.js
@@ -2,8 +2,8 @@ Ext.Loader.setPath('Ext', 'extjs/src');
 Ext.Loader.setConfig({enabled: true});
 
 Ext.application({
-    name: 'IM',         		//app namespace (from Issue Manager)
-	models: [],                 //here goes the models
-    controllers: [],            //here goes the controllers
-    autoCreateViewport: true    //automatically creates Viewport
+    name 				: 'IM',   //app namespace (from Issue Manager)
+    autoCreateViewport	: true,   //automatically creates Viewport
+	models				: [],     //here goes the models
+    controllers			: []     //here goes the controllers
 });
--- public/app/view/Viewport.js
+++ public/app/view/Viewport.js
@@ -1,3 +1,21 @@
 Ext.define('IM.view.Viewport', {
-    extend: 'Ext.container.Viewport'
+    extend: 'Ext.container.Viewport',
+
+    layout: 'border',
+ 
+    items	: [{
+        xtype	: 'container',
+        region	: 'center',
+        layout	: 'anchor',
+        defaults	: {
+			xtype	: 'button',
+			height	: 50,
+			anchor	: '0'
+		},
+		items	: [{
+		    	text	: 'Criar projeto'
+		    },{
+		    	text	: 'Acessar projeto'
+		}]
+    }]
 });
