3535b1b482e7c727f97c06996c2f6a41f6fa95da
Adriano Flach <flachadriano@gmail.com>
2012-03-24 16:59:04 -0300

    adjust screen tenderization

--- public/app/controller/Controller.js
+++ public/app/controller/Controller.js
@@ -1,11 +1,11 @@
 Ext.define('IM.controller.Controller', {
-    extend  : 'Ext.app.Controller',
+    extend	: 'Ext.app.Controller',
 
     render: function(view) {
         var container 	= Ext.getCmp('im_container'),
         	win     	= Ext.widget(view);
         
         container.removeAll();
-        container.add(tab);
+        container.add(win);
     }
 });
